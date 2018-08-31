0228dcc8b4f784a36cadf554b25b8e923e9a3edc
Paul Schlueter <paul@paulschlueter.com>
2018-08-31 01:09:08 -0500

    Got Dummy Bokeh Plot Working

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
-from database import BillInfo
+from flask import Flask, render_template
+from bokeh.embed import components
+from database import BillInfo, ExecutionStatus
+from bokeh.plotting import figure
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello'
+    p = figure()
+    p.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
+
+    script, div = components(p)
+
+    return render_template('dashboard.html', exec_status_script=script, exec_status_div=div)
 
 
 @app.route('/data/')
