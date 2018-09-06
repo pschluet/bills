6a05f293e35a0eac7fe68abf7a95334955b51c50
Paul Schlueter <paul@paulschlueter.com>
2018-09-05 21:00:50 -0500

    Update Scraper launchd Config to Run Once Daily

--- RunScrapers.sh
+++ RunScrapers.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # To run this repeatedly using launchd enter the following in the terminal:
 # launchctl load ~/Library/LaunchAgents/com.ps.billscrapers.plist 
 source /Users/schlueter/PycharmProjects/bills/venv/bin/activate && /Users/schlueter/PycharmProjects/bills/venv/bin/python /Users/schlueter/PycharmProjects/bills/scrapers.py && deactivate
